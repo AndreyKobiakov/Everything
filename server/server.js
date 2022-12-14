07ab0bc511a038a66684576e7531fb4ab90ad2cb
Andrey K <coke@list.ru>
2022-12-14 11:27:17 +0300

    server initialization

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const postsRouter = require('./routes/postsRouter');
+
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(cors({
+  credentials: true,
+  origin: true,
+}));
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/v1/', postsRouter);
+
+app.listen(PORT, () => console.log(`Server has started on PORT ${PORT}`));
