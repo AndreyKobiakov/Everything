46a9e76cb46f811005b3d773e9d290baefaa7a2c
Andrey K <coke@list.ru>
2022-12-14 12:00:29 +0300

    first db

---
+++ server/db/models/post.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Post extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      this.belongsTo(models.User, { foreignKey: 'user_id' });
+      this.hasMany(models.Like, { foreignKey: 'post_id' });
+    }
+  }
+  Post.init({
+    post: DataTypes.STRING,
+    user_id: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Post',
+  });
+  return Post;
+};
